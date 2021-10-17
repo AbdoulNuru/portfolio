bf4eea5175e5347c183b7bf17f66ed0274f81fb7
AbdoulNuru <abdoulniyigena@gmail.com>
2021-10-17 21:01:27 +0200

    ft: add work and contact me section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -530,6 +530,220 @@ const Home = () => {
       </section>
 
       {/* Aho ubuhanga burangirira */}
+
+      {/* Akazi kanjye */}
+      <section className="section bg-light" id="projects">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-12 text-center">
+              <div className="container-title text-center mb-4 pb-2">
+                <div className="titles">
+                  <h2 className="title text-capitalize mb-4">My Work</h2>
+                  <p className="pera-title para-desc-600 text-light-muted mb-0 mx-auto">
+                    Obviously I'm a Web Designer. Experienced with all stages of
+                    the development cycle for dynamic web projects.
+                  </p>
+                  <span></span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-lg-4 col-md-6 col-12 mt-4 pt-2">
+              <div className="card border-0 work-container work-modern position-relative d-block overflow-hidden rounded">
+                <div className="card-body p-0">
+                  <img
+                    src="img/comingsoon.jpg"
+                    className="img-fluid"
+                    alt="work"
+                  />
+                  <div className="overlay-work bg-dark"></div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6 col-12 mt-4 pt-2">
+              <div className="card border-0 work-container work-modern position-relative d-block overflow-hidden rounded">
+                <div className="card-body p-0">
+                  <img src="img/comingsoon.jpg" class="img-fluid" alt="work" />
+                  <div className="overlay-work bg-dark"></div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6 col-12 mt-4 pt-2">
+              <div className="card border-0 work-container work-modern position-relative d-block overflow-hidden rounded">
+                <div className="card-body p-0">
+                  <img
+                    src="img/comingsoon.jpg"
+                    className="img-fluid"
+                    alt="work"
+                  />
+                  <div className="overlay-work bg-dark"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Aho akazi kanjye karangirira */}
+
+      {/* Nyandikira */}
+      <section className="section pb-0" id="contact">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-12 text-center">
+              <div className="container-title text-center mb-4 pb-2">
+                <div className="titles">
+                  <h2 className="title text-capitalize mb-4">Contact Me</h2>
+                  <p className="pera-title para-desc-600 text-light-muted mb-0 mx-auto">
+                    Obviously I'm a Web Designer. Experienced with all stages of
+                    the development cycle for dynamic web projects.
+                  </p>
+                  <span></span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-4 mt-4 pt-2">
+              <div className="contact-detail text-center">
+                <div className="icon">
+                  <i data-feather="phone" className="fea icon-md"></i>
+                </div>
+                <div className="content mt-4">
+                  <h5 className="title">Phone</h5>
+                  <p className="text-light-muted">
+                    Promising development turmoil inclusive education
+                    transformative community
+                  </p>
+                  <a href="tel:+152534-468-854" className="text-primary">
+                    +152 534-468-854
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-4 mt-4 pt-2">
+              <div className="contact-detail text-center">
+                <div className="icon">
+                  <i data-feather="mail" className="fea icon-md"></i>
+                </div>
+                <div className="content mt-4">
+                  <h5 className="title">Email</h5>
+                  <p className="text-light-muted">
+                    Promising development turmoil inclusive education
+                    transformative community
+                  </p>
+                  <a href="mailto:contact@example.com" className="text-primary">
+                    contact@example.com
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-4 mt-4 pt-2">
+              <div className="contact-detail text-center">
+                <div className="icon">
+                  <i data-feather="map-pin" className="fea icon-md"></i>
+                </div>
+                <div class="content mt-4">
+                  <h5 className="title">Location</h5>
+                  <p className="text-light-muted">
+                    C/54 Northwest Freeway, Suite 558, Houston, USA 485
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="section pt-5">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-12">
+              <div className="custom-form mb-sm-30">
+                <form
+                  method="post"
+                  action=""
+                  name="myForm"
+                  onsubmit="return validateForm()"
+                >
+                  <p id="error-msg"></p>
+                  <div id="simple-msg"></div>
+                  <div className="row">
+                    <div className="col-lg-6">
+                      <div className="row">
+                        <div className="col-lg-12 col-md-6">
+                          <div className="form-group">
+                            <input
+                              name="name"
+                              id="name"
+                              type="text"
+                              className="form-control border rounded"
+                              placeholder="First Name :"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-lg-12 col-md-6">
+                          <div className="form-group">
+                            <input
+                              name="email"
+                              id="email"
+                              type="email"
+                              className="form-control border rounded"
+                              placeholder="Your email :"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-lg-12">
+                          <div className="form-group">
+                            <input
+                              name="subject"
+                              id="subject"
+                              className="form-control border rounded"
+                              placeholder="Your subject :"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="col-lg-6">
+                      <div className="form-group">
+                        <textarea
+                          name="comments"
+                          id="comments"
+                          rows="4"
+                          className="form-control border rounded"
+                          placeholder="Your Message :"
+                        ></textarea>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-sm-12 text-end">
+                      <input
+                        type="submit"
+                        id="submit"
+                        name="send"
+                        className="submitBnt btn btn btn-primary rounded"
+                        value="Send Message"
+                      />
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Aho nyandikira irangirira */}
     </>
   );
 };
