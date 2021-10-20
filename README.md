71a20899be1201fdc6c67e58dd4440c21b29b059
AbdoulNuru <abdoulniyigena@gmail.com>
2021-10-20 14:03:16 +0200

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# My Portfolio
-
-# Description ✍︎
---
+++ precache-manifest.85d8924b3de64f569aa0c33ef1a46824.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b193fb5e844ff8f126e094ca968e9b60",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "28ce544f057c49f771be",
+    "url": "/My-Portfolio/static/js/2.57c13b5a.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/My-Portfolio/static/js/2.57c13b5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efc2e458c95988d0c960",
+    "url": "/My-Portfolio/static/js/main.0847eaef.chunk.js"
+  },
+  {
+    "revision": "d83d1f30d43280c7bcfb",
+    "url": "/My-Portfolio/static/js/runtime-main.2848297b.js"
+  },
+  {
+    "revision": "0110df129500d5f753e00b17fffe7763",
+    "url": "/My-Portfolio/static/media/Abdou Nuru Resume.0110df12.pdf"
+  }
+]);
