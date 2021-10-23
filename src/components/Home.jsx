a83983e48554e33f3d72a292203f7b38ebb5e1fb
AbdoulNuru <abdoulniyigena@gmail.com>
2021-10-23 11:33:24 +0200

    ft: refactor the styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -27,17 +27,6 @@ const Home = () => {
 
   return (
     <>
-      {/* Loader */}
-      {/* <div className="preloader">
-        <div className="status">
-          <div className="spinner">
-            <div className="double-bounce1"></div>
-            <div className="double-bounce2"></div>
-          </div>
-        </div>
-      </div> */}
-      {/* Loader */}
-      {/* Navbar */}
       {loading === false ? (
         <LoadingScreen />
       ) : (
